9b4bb35b3a56abf6bad8f504b486aa1814767dc0
HAHYEM <sky00gpals@naver.com>
2020-11-03 17:22:53 +0900

    EditText and Event

--- part2_3/src/main/java/com/example/part2_3/Lab3_2Activity.java
+++ part2_3/src/main/java/com/example/part2_3/Lab3_2Activity.java
@@ -1,14 +1,31 @@
 package com.example.part2_3;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Lab3_2Activity extends AppCompatActivity {
 
+    CheckBox checkBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_2);
+        
+        checkBox = findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkBox.setText("this is Checked");
+                } else {
+                    checkBox.setText("this is unChecked");
+                }
+            }
+        });
     }
+
+
 }
